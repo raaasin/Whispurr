f5eea789d7e82ca32c4e66f6b0d272548eb4d8e5
raaasin <nisarvskp@gmail.com>
2023-10-16 20:55:47 +0530

    changes

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-large")
 # Let's chat for 5 lines
 for step in range(5):
     # encode the new user input, add the eos_token and return a tensor in Pytorch
-    new_user_input_ids = tokenizer.encode(input(">> User:") + tokenizer.eos_token, return_tensors='pt')
+    new_user_input_ids = tokenizer.encode(input(">> Nisar:") + tokenizer.eos_token, return_tensors='pt')
 
     # append the new user input tokens to the chat history
     bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
